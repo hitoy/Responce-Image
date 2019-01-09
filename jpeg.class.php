3f71d1a8df6aaf9c1de93d057a665c081c594460
Hito Yang <vip@hitoy.org>
2019-01-09 09:40:16 +0800

    bug修复

--- jpeg.class.php
+++ jpeg.class.php
@@ -8,105 +8,233 @@
  * http://vip.sugovica.hu/Sardi/kepnezo/JPEG%20File%20Layout%20and%20Format.htm
  */
 class JPEG{
-    //JPEG Metadata Structure
-    private $markerkey=array("soi"=>0xd8,"sof0"=>0xc0,"sof2"=>0xc2,"dht"=>0xc4,"dqt"=>0xdb,"dri"=>0xdd,"sos"=>0xda,"rst"=>array(0xd0,0xd1,0xd2,0xd3,0xd4,0xd5,0xd6,0xd7),"app"=>array(0xe0,0xe1,0xe2,0xe3,0xe4,0xe5,0xe6,0xe7,0xe8,0xe9,0xea,0xeb,0xec,0xed,0xee,0xef),"com"=>0xfe,"eoi"=>0xd9);
-    private $markerdata=array();
-    private $ImageScan;
-    private $Image;
-    private $PosPointer;
-    private $ImageSize;
-    public function __construct($argument){
-        if(file_exists($argument)){
-            $this->Image = fopen($argument,"rb+");
-            $this->ImageSize= filesize($argument);
-        }else{
-            die("File Not Exists");
+    //JPEG开始标记
+    public $marker = 0xd8ff;
+
+    //原始二级制数据
+    public $rawData;
+
+    //文件大小，单位字节
+    public $size = 0;
+
+    //是否时JPEG格式
+    public $isJPEG = false;
+
+    //JPEG图片头部格式的标记符名称和对应字节码
+    public $headermarker = array("soi"=>0xd8,"sof0"=>0xc0,"sof2"=>0xc2,"dht"=>0xc4,"dqt"=>0xdb,"dri"=>0xdd,"sos"=>0xda,"rst"=>array(0xd0,0xd1,0xd2,0xd3,0xd4,0xd5,0xd6,0xd7),"app"=>array(0xe0,0xe1,0xe2,0xe3,0xe4,0xe5,0xe6,0xe7,0xe8,0xe9,0xea,0xeb,0xec,0xed,0xee,0xef),"com"=>0xfe,"eoi"=>0xd9);
+
+    //JPEG头部数据
+    public $headerData = array();
+
+    //JPEG内容
+    public $scanData;
+
+    //当前文件
+    private $file;
+
+    //当前指针位置
+    private $position = 0;
+
+    /*
+     * 初始化图片类型
+     * @param $file string 文件路径或者二进制数据
+     */
+    public function __construct($file = NULL){
+        if(@file_exists($file)){
+            $this->rawData = file_get_contents($file);
+            $this->size = filesize($file);
+            $this->file = $file;
+        }elseif(is_string($file)){
+            $this->rawData = $file;
+            $this->size = strlen($file); 
         }
-        if($this->GetByte(2)!==0xd8ff){
-            die("Not a JPEG Image");
+
+        if($this->getbyte(2) == $this->marker){
+            $this->isJPEG = true;
+            $this->parse();
         }
-        $this->Parse();
     }
 
-    public function GetByte($len){
-        if(feof($this->Image)) return NULL;
-        $tmp =  fread($this->Image,$len);
-        $tmp2=array();
-        $this->PosPointer=ftell($this->Image);
-        if($len==1){
-            $tmp2=unpack("C",$tmp);
-        }else if($len==2){
-            $tmp2=unpack("S",$tmp);
-        }else if($len==4){
-            $tmp2=unpack("L",$tmp);
-        }else{
-            $tmp2[1]=$tmp;
-        }
-        return $tmp2[1];
+    /*
+     * 获取指定长度的字节信息
+     * @param $length int 长度
+     * @param $postion int|null 开始的位置，默认从上次的位置开始
+     * @return string|false 成功返回二级制字节信息，失败返回false
+     */
+    public function getbyte($length = 1, $position = NULL){
+        $output = array();
+
+        if($length > $this->size - $this->position)
+            return false;
+
+        if($position == NULL)
+            $position = $this->position;
+
+        $data = substr($this->rawData,$position,$length);
+        $this->position += $length;
+
+        if($length == 1)
+            $output = unpack("C",$data);
+        else if($length == 2)
+            $output = unpack("S",$data);
+        else if($length == 4)
+            $output = unpack("L",$data);
+        else
+            $output[1] = $data;
+
+        return $output[1];
     }
-    public function GetMarkerData(){
-        $buffer="";
-        foreach($this->markerdata as $d){
+
+    /*
+     * 根据字节获取二进制获取标记名称
+     */
+    public function get_marker($byte){
+        foreach($this->headermarker as $name=>$bit){
+            if(is_array($bit)){
+                if(in_array($byte,$bit)) return $name;
+            }else{
+                if($byte == $bit) return $name;
+            }
+        }
+        return NULL;
+    }
+
+    /*
+     * 解析JPEG数据，把各个分段数据存储到数组结构中
+     */
+    public function parse(){
+        if($this->isJPEG == false) return false;
+
+        while(($byte = $this->getbyte()) !== false):
+            if($byte == 0xff):
+                $nxtbyte = $this->getbyte();
+                $marker = $this->get_marker($nxtbyte);
+
+                if($marker == 'eoi'){
+                    break;
+                //可变长度的字段
+                }elseif($marker == 'sof0' || $marker == 'sof2' || $marker == 'dht' || $marker == 'dqt' || $marker == 'app' || $marker == 'com'){
+                    $datalen = $this->getbyte(1)*256 + $this->getbyte(1) - 2;
+                    $data = $this->getbyte($datalen);
+                    $headerdata = $data;
+                    $this->headerData[$nxtbyte][]= $headerdata;
+                }
+                //DRI
+                elseif($marker == 'dri'){
+                    $this->headerData[$nxtbyte][] = $this->getbyte(4);
+                }
+                //rst
+                elseif($marker == 'rst'){
+                    $this->headerData[$nxtbyte][] = '';
+                }
+                //JPEG主体数据
+                elseif($marker == 'sos'){
+                    while(($byte = $this->getbyte()) !== false){
+                        if($byte == 0xff){
+                            $nxtbyte = $this->getbyte();
+                            if($nxtbyte == 0xd9) break;
+                            else $this->scanData .= pack('C',$byte).pack('C',$nxtbyte);
+                        }else{
+                            $this->scanData .= pack('C',$byte);
+                        }
+                    }
+                }
+            endif;
+        endwhile;
+    }
+
+    /*
+     * 根据JPEG的所有头部二进制数据
+     */
+    public function getheaderdata(){
+        $buffer = "";
+        foreach($this->headerData as $d){
             foreach($d as $c){
-                $buffer.=$c;
+                $buffer .= $c;
             }
         }
         return $buffer;
     }
 
-    public function get_marker($byte){
-        foreach($this->markerkey as $meta=>$bit){
-            if(is_array($bit)){
-                foreach($bit as $b){
-                    if($byte == $b) return $meta;
-                }
-            }
-            if($byte == $bit) return $meta;
-        }
-        return NULL;
-    }
-
+    /*
+     * 压缩图片
+     */
     public function compress(){
-        //Remove Com Marker
-        $this->markerdata['com']=array();
-        //Remove App Marker
-        $this->markerdata['app']=array();
+        $this->clear_com();
+        $this->clear_app();
     }
 
-    public function Parse(){
-        while(($byte=$this->GetByte(1))!==NULL){
-            $nxtbyte = @$this->GetByte(1);
-            if($byte == 0xff){
-                $marker = $this->get_marker($nxtbyte);
-                //Variable Size Marker
-                if($marker == "sof" || $marker == "sof2" || $marker == "dht" || $marker == "dqt" || $marker == "sof0" || $marker == "app" || $marker == "com"){
-                    $offset1= $this->GetByte(1);
-                    $offset2 = $this->GetByte(1);
-                    $len = $offset1*256+$offset2;
-                    $data = $this->GetByte($len-2);
-                    //full data include marker and length
-                    $fulldata = pack("C",0xff).pack("C",$nxtbyte).pack("C",$offset1).pack("C",$offset2).$data;
-                    $this->markerdata[$marker][]= $fulldata;
-                }else if($marker=="dri"){
-                    //pass
-                }else if($marker=="rst"){
-                    //pass
-                }else if($marker=="sos"){
-                    //SOS Segment
-                    $len = $this->ImageSize-$this->PosPointer-1;
-                    $this->ImageScan =  pack("C",0xff). pack("C",0xda).$this->GetByte($len);
-                }
+    /*
+     * 移除JPEG COM数据段
+     */
+    public function clear_com(){
+        unset($this->headerData[0xfe]);
+    }
+
+    /*
+     * 移除JPEG的app段
+     * 由于JFIF在0xe0上，这段不予移除
+     */
+    public function clear_app(){
+        foreach($this->headermarker['app'] as $code)
+            if($code != 0xe0)
+                unset($this->headerData[$code]);
+    }
+
+    /*
+     * 增加JPEG COM数据段
+     */
+    public function add_com($string){
+        $this->headerData[0xfe][] = $string;
+        return true;
+    }
+
+    /*
+     * 增加JPEG APP数据段
+     */
+    public function add_app($string){
+        $appmarker = 0xe1;
+        foreach($this->headerData as $markercode=>$datas){
+            if(in_array($markercode,$this->headermarker['app']))
+                $appmarker++;
+        }
+        if($appmarker > 0xef)
+            return false;
+        $this->headerData[$appmarker][] = $string;
+        return true;
+    }
+
+    /*
+     * 获取压缩之后的二进制数组
+     */
+    public function getbin(){
+        if(!$this->isJPEG)
+            return $this->rawData;
+        $bin = pack('S',$this->marker);
+        foreach($this->headerData as $markercode=>$datas){
+            foreach($datas as $data){
+                $datalen = strlen($data) + 2;
+                $markername = $this->get_marker($markercode);
+                if(in_array($markername,array('sof0','sof2','dht','dqt','app','com')))
+                    $bin .= pack('C',0xff).pack('C',$markercode).pack('n',$datalen).$data;
+                elseif($markername == 'dri')
+                    $bin .= pack('C',0xff).pack('C',$markercode).pack('L',$data);
+                elseif($markername == 'rst')
+                    $bin .= pack('C',0xff).pack('C',$markercode).$data;
             }
         }
+        $bin .= pack('C',0xff).pack('C',0xda).$this->scanData.pack('C',0xff).pack('C',0xd9);
+        return $bin;
     }
-    public function GetImageBin(){
-        return pack("S",0xd8ff).$this->GetMarkerData().$this->ImageScan.pack("S",0xd9ff);
-    }
-    public function Storage(){
-        fseek($this->Image,0);
-        fwrite($this->Image,$this->GetImageBin());
-    }
-    public function __destruct(){
-        fclose($this->Image);
+
+    /*
+     * 覆盖保存原有图片
+     */
+    public function storage(){
+        if(!empty($this->file)){
+            $data = $this->getbin();
+            return file_put_contents($this->file, $data, EX_LOCK);
+        }
+        return false;
     }
 }
